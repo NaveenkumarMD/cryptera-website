91dea1e148fef25969cfb5e517f293b21d9b0baa
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-01-17 14:25:43 +0530

    home

--- src/Components/Screens/Home.jsx
+++ src/Components/Screens/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react'
 import '../../Styles/Home.css'
+import * as LottiePlayer from '@lottiefiles/lottie-player'
 import { Navigate, useNavigate } from 'react-router-dom'
+import Button from '../../Components/Button'
 function Home() {
     const navigator = useNavigate()
     const [flag, setFlag] = React.useState(false)
@@ -12,7 +14,7 @@ function Home() {
         console.log('wheel')
         console.log(event.deltaY)
         if (event.deltaY < 0) {
-            h=0
+            h = 0
             greenref.current.style.height = "0px"
         }
         else {
@@ -28,12 +30,24 @@ function Home() {
     }
     return (
         <div className='home main-section' id="Home" ref={Homeref} onWheel={wheelhandler}>
-            <div className='py-56'>hello </div>
-            <div className='py-56'>hello </div>
-
-            <div className='add-green bg-gradient-to-tr from-green-600 to-green-300 ' ref={greenref}>
+            <div className='home-content'>
+                <div className='home-content-text'>
+                    <h1>A National level Technical Symposium conducted by CSE department of CIT.    </h1>
+                    <Button text="Check out the Events"/>
+                </div>
+                <div>
+                    <lottie-player
+                        autoplay
+                        loop
+                        mode="normal"
+                        src="https://assets5.lottiefiles.com/packages/lf20_as1vcloj.json"
+                    ></lottie-player>
+                </div>
 
             </div>
+            <div className='add-green bg-gradient-to-tr from-green-600 to-green-300 ' ref={greenref} />
+
+
 
 
 
